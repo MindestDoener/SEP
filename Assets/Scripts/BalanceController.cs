54efa46e9e830217b8aa5bac692c1b64a7cd9a74
dreamz1e <61279526+dreamz1e@users.noreply.github.com>
2020-08-27 21:39:36 +0200

    Balance system (#6)
    * Added BalanceController
    
    * Added ResetBalance()
    
    * Fix from Pull Request

---
+++ Assets/Scripts/BalanceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceController : MonoBehaviour
+{
+    
+    private static decimal _balance;
+    private GameObject TextField;
+    private BalanceDisplayController bdc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextField = GameObject.FindWithTag("Balance");
+        bdc = (BalanceDisplayController) TextField.GetComponent(typeof(BalanceDisplayController));
+
+    }
+    
+    public void AddBalance(decimal amount) 
+    {
+        _balance += amount;
+        UpdateBalanceDisplay();
+    }
+
+    public void ResetBalance() 
+    {
+        _balance = 0;
+        UpdateBalanceDisplay();
+    }
+
+    private void UpdateBalanceDisplay() 
+    {
+        bdc.DisplayBalance(_balance);
+    }
+}
