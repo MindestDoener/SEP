a520e9dad064b49528d54802d1cd2d546741ca36
dreamz1e <robinseifert0408@gmail.com>
2020-08-27 17:23:20 +0200

    Added BalanceController

---
+++ Assets/Scripts/BalanceController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceController : MonoBehaviour
+{
+    
+    private static decimal _balance;
+    GameObject TextField;
+    BalanceDisplayController bdc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextField = GameObject.FindWithTag("Balance");
+        bdc = (BalanceDisplayController) TextField.GetComponent(typeof(BalanceDisplayController));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AddBalance(decimal amount) 
+    {
+        _balance += amount;
+        UpdateBalanceDisplay();
+    }
+
+    private void UpdateBalanceDisplay() 
+    {
+        bdc.DisplayBalance(_balance);
+    }
+}
---
+++ Assets/Scripts/BalanceDisplayController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BalanceDisplayController : MonoBehaviour
+{
+    Text balanceText;
+    private string _suffix;
+    private decimal _divisor;
+    private int _exp;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balanceText = GetComponent<Text>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayBalance(decimal balance) 
+    {
+        balanceText.text = ShortenBalanceDisplay(balance);
+    }
+
+    private string ShortenBalanceDisplay(decimal balance) 
+    {
+        _divisor = Convert.ToDecimal(Math.Pow(10, _exp));
+
+        if (balance >= Convert.ToDecimal(Math.Pow(10, 3)) && balance < Convert.ToDecimal(Math.Pow(10, 6))) {
+            _suffix = " K";
+            _exp = 3;
+        }
+        else if (balance >= Convert.ToDecimal(Math.Pow(10, 6)) && balance < Convert.ToDecimal(Math.Pow(10, 9))) 
+        {
+            _suffix = " M";
+            _exp = 6;
+        }
+        else if (balance >= Convert.ToDecimal(Math.Pow(10, 9)) && balance < Convert.ToDecimal(Math.Pow(10, 12))) 
+        {
+            _suffix = " B";
+            _exp = 9;
+        }
+        else if (balance >= Convert.ToDecimal(Math.Pow(10, 12)) && balance < Convert.ToDecimal(Math.Pow(10, 15))) 
+        {
+            _suffix = " T";
+            _exp = 12;
+        }
+
+        return Math.Round(balance/_divisor, 2).ToString() + _suffix;
+       
+
+
+    }
+}
