156a314c29261f54ae98de9b733c2962acc5e984
Justus <Justernst.je@gmail.com>
2020-08-28 13:18:20 +0200

    Player Prefab
    Player Prefab includes CircleRenderer which Draws a Polygon with a variable number of points with a varaible Radius. Radius should be set in Player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CircleRendererController circleRenderer;
+    [SerializeField] private float collectionRadius = 1f;
+    [SerializeField] private float passiveCollectionMultiplier;
+    [SerializeField] private float activeCollectionMultiplier;
+
+    public float PassiveCollectionMultiplier
+    {
+        get => passiveCollectionMultiplier;
+        set => passiveCollectionMultiplier = value;
+    }
+
+    public float ActiveCollectionMultiplier
+    {
+        get => activeCollectionMultiplier;
+        set => activeCollectionMultiplier = value;
+    }
+    public float CollectionRadius
+    {
+        get => collectionRadius;
+        set => collectionRadius = value;
+    }
+
+    public void UpdateCollectionRadius(float factor)
+    {
+        collectionRadius *= factor;
+    }
+
+    private void Update()
+    {
+        if (Math.Abs(circleRenderer.Radius - collectionRadius) > 0.1f)
+        { 
+            circleRenderer.Radius = collectionRadius; 
+        }
+        
+    }
+    
+    public bool IsInReach(Vector2 coordinates)
+    {
+        var distanceVector = (Vector2) transform.position - coordinates;
+
+        return distanceVector.magnitude <= collectionRadius;
+    }
+}
