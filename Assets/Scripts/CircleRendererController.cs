03781938834d91ff93b9715516b347b3341da525
Justus-e <56258792+Justus-e@users.noreply.github.com>
2020-09-01 17:14:22 +0200

    Features/player prefab (#7)
    * Player Prefab
    
    Player Prefab includes CircleRenderer which Draws a Polygon with a variable number of points with a varaible Radius. Radius should be set in Player.
    
    * Circle Renderer
    
    Renders Circle for Collection Radius
    
    +CodeCleanup
    
    * fixed Variable accessors
    
    Co-authored-by: Justus <Justernst.je@gmail.com>

---
+++ Assets/Scripts/CircleRendererController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class CircleRendererController : MonoBehaviour
+{
+    private const float CircleDegree = 360f;
+    [SerializeField] private int pointCount = 36;
+    [SerializeField] private Material material;
+    private float _radius;
+
+    public float Radius
+    {
+        get => _radius;
+        set
+        {
+            _radius = value;
+            RenderCircle();
+        }
+    }
+
+    private void RenderCircle()
+    {
+        var mesh = new Mesh();
+        var vertices = new Vector3[pointCount + 1];
+
+        var angle = CircleDegree / pointCount;
+
+        for (var i = 0; i < pointCount; i++)
+        {
+            var x = Math.Cos(DegToArc(angle * i)) * _radius;
+            var y = Math.Sin(DegToArc(angle * i)) * _radius;
+            vertices[i] = new Vector3((float) x, (float) y);
+        }
+
+        vertices[pointCount] = new Vector3(0f, 0f);
+
+        mesh.vertices = vertices;
+
+        var triangles = new int[pointCount * 3];
+
+        for (var i = 0; i < pointCount; i++)
+        {
+            triangles[3 * i] = pointCount;
+            triangles[3 * i + 1] = i;
+            if (i < pointCount - 1)
+                triangles[3 * i + 2] = i + 1;
+            else
+                triangles[3 * i + 2] = 0;
+        }
+
+        mesh.triangles = triangles;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().material = material;
+        Debug.Log("Circle rendered");
+    }
+
+    private static double DegToArc(float angle)
+    {
+        return angle * Math.PI / 180;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CircleRendererController circleRenderer;
+    [SerializeField] private float collectionRadius = 1f;
+    [SerializeField] private float passiveCollectionMultiplier;
+    [SerializeField] private float activeCollectionMultiplier;
+
+    public float PassiveCollectionMultiplier
+    {
+        get => passiveCollectionMultiplier;
+        set => passiveCollectionMultiplier = value;
+    }
+
+    public float ActiveCollectionMultiplier
+    {
+        get => activeCollectionMultiplier;
+        set => activeCollectionMultiplier = value;
+    }
+
+    public float CollectionRadius
+    {
+        get => collectionRadius;
+        set => collectionRadius = value;
+    }
+
+    private void Update()
+    {
+        if (Math.Abs(circleRenderer.Radius - collectionRadius) > 0.1f) circleRenderer.Radius = collectionRadius;
+    }
+
+    public void UpdateCollectionRadius(float factor)
+    {
+        collectionRadius *= factor;
+    }
+
+    public bool IsInReach(Vector2 coordinates)
+    {
+        var distanceVector = (Vector2) transform.position - coordinates;
+
+        return distanceVector.magnitude <= collectionRadius;
+    }
+}
