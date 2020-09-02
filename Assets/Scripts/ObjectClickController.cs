11d77349d7a08ced8c447766e35cc8aa3b65c27e
dreamz1e <robinseifert0408@gmail.com>
2020-09-02 12:22:02 +0200

    Added Destroying GameObject on click

---
+++ Assets/Scripts/ObjectClickController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickController : MonoBehaviour
+{
+    private RaycastHit2D _hit;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        ClickToDestroy();
+    }
+
+    private void ClickToDestroy()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+      
+            if (_hit.collider != null)
+            {
+                Destroy(_hit.collider.gameObject);
+            }  
+        }
+        
+    }
+}
