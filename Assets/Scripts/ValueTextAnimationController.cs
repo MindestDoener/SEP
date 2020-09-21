79de8326e48dd98a6d522f297115315d681b615c
EagleEars <60156681+EagleEars@users.noreply.github.com>
2020-09-21 11:03:01 +0200

    Adding that every clicked trash gets their value displayed

---
+++ Assets/Scripts/ValueTextAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+class ValueTextAnimationController : MonoBehaviour
+{
+    float i = 1;
+    private void Start()
+    {
+        StartCoroutine(FadingAnimation());
+    }
+
+    IEnumerator FadingAnimation()
+    {
+        while(i > 0)
+        {
+            Color color = Color.white;
+            color.a = i;
+            var fadingColor = color;
+            GetComponent<TextMesh>().color = fadingColor;
+            i = i - 0.01f;
+            yield return new WaitForSeconds(0.02f);
+        }
+        Destroy(this.gameObject);
+    }
+}
+
+    
+
