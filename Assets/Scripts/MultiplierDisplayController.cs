3757a5b4f6c87c026446ad9c6c3159fe341012a1
dreamz1e <robinseifert0408@gmail.com>
2020-09-24 15:47:42 +0200

    Multiplier now shows values with commata

--- Assets/Scripts/MultiplierDisplayController.cs
+++ Assets/Scripts/MultiplierDisplayController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -10,6 +11,9 @@ public class MultiplierDisplayController : MonoBehaviour
 
     private ObjectClickController _occ;
     private BalanceDisplayController _bdc;
+    private string _suffix;
+    private decimal _divisor;
+    private int _exp;
 
     private Text _multiplierDisplay;
     // Start is called before the first frame update
@@ -18,13 +22,59 @@ public class MultiplierDisplayController : MonoBehaviour
         _cam = Camera.main;
         _multiplierDisplay = GetComponent<Text>();
         _occ = _cam.GetComponent<ObjectClickController>();
-        _bdc = GetComponent<BalanceDisplayController>();
     }
 
     // Update is called once per frame
 
     public void UpdateDisplay()
     {
-        _multiplierDisplay.text = _bdc.ShortenBalanceDisplay(_occ.GetMultiplier()) + "x";
+        _multiplierDisplay.text = ShortenMultiplierDisplay(_occ.GetMultiplier()) + "x";
+    }
+    
+    public string ShortenMultiplierDisplay(decimal multiplier) 
+    {
+
+        if (multiplier >= GetPower(10, 3) && multiplier < GetPower(10, 6)) {
+            _suffix = "K";
+            _exp = 3;
+        }
+        else if (multiplier >= GetPower(10, 6) && multiplier < GetPower(10, 9)) 
+        {
+            _suffix = "M";
+            _exp = 6;
+        }
+        else if (multiplier >= GetPower(10, 9) && multiplier < GetPower(10, 12)) 
+        {
+            _suffix = "B";
+            _exp = 9;
+        }
+        else if (multiplier >= GetPower(10, 12) && multiplier < GetPower(10, 15)) 
+        {
+            _suffix = "T";
+            _exp = 12;
+        }
+        else
+        {
+            _suffix = "";
+            _exp = 0;
+        }
+
+        _divisor = GetPower(10, _exp);
+
+        if (_exp > 0)
+        {
+            return Math.Round(multiplier/_divisor, 2) + " " + _suffix;
+        }
+        else
+        {
+            return (multiplier / _divisor) + " " + _suffix;
+        }
+
+
+    }
+    
+    private decimal GetPower(int a, int b)
+    {
+        return Convert.ToDecimal(Math.Pow(a, b));
     }
 }
