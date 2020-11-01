837d43133b46c9fb2d5f0cbf9c2951f2a85819f5
EagleEars <60156681+EagleEars@users.noreply.github.com>
2020-11-01 14:39:04 +0100

    Features/char customizer (#34)
    * LeftUI
    
    CharCustomizer is sliding in when clicking on Player. CC is hiding when hiting the closeButton. PlayerModel is seperated in body parts.
    
    * Character Customizer is working with test Textures
    
    Character Customizer has a task bar where you can choose between different parts of the body. By clicking on the clothes the look of the player is changing.
    
    * NavButtons added
    
    Added 2 Buttons that can navigate through the items. Also changed the classnames and general overlay of the left UI.
    
    * Changed TabBar
    
    Added Background to TabBarButtons.
    
    * Added Textures and BugFixes
    
    New textures for pants, faces and hats. Added ExceptionHandler to CurrentTab function.
    
    * fix shop animation
    
    * added Customizer to the right UI
    
    * Fixed that customizable items are out of the box
    
    * Revert "Fixed that customizable items are out of the box"
    
    This reverts commit a93923b263de6b731fb08a795d94c7e3d201dc47.
    
    * Fix CharCustomizer
    
    fix scaling issue
    
    Co-authored-by: Justus <Justernst.je@gmail.com>
    Co-authored-by: Justus-e <56258792+Justus-e@users.noreply.github.com>

---
+++ Assets/ScriptableObjects/WearableItemScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class WearableItemScriptableObject : ScriptableObject
+{
+    public String Name;
+    public Sprite ItemImage;
+    public bool IsUnlocked = false;
+    public WearableItem ItemType;
+}
---
+++ Assets/Scripts/TabItemController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    internal class TabItemController : MonoBehaviour
+    {
+        [SerializeField] private GameObject ItemPrefab;
+        [SerializeField] private WearableItem ItemType; //muss einem Ordner aus Resources/WearableItems gleichen
+        private GameObject CurrentItem;
+        private readonly int firstItemIndex = 0;
+
+        public void LoadItems(int NavButtonInput)
+        {
+            var WearableItems = Resources.LoadAll<WearableItemScriptableObject>("WearableItems\\" + ItemType);
+            var ItemContainer = GameObject.FindWithTag("ItemContainer");
+
+            foreach (Transform child in ItemContainer.transform) Destroy(child.gameObject);
+
+            for (var i = 0; i < WearableItems.Length; i++)
+            {
+                CurrentItem = Instantiate(ItemPrefab, ItemContainer.transform);
+                CurrentItem.transform.GetChild(0).GetComponent<Image>().sprite = WearableItems[i].ItemImage;
+                CurrentItem.transform.GetChild(1).GetComponent<Text>().text = WearableItems[i].Name;
+                CurrentItem.GetComponent<WearableItemController>().ItemType = ItemType;
+            }
+        }
+
+        public float GetScale(WearableItem ItemType)
+        {
+            switch (ItemType)
+            {
+                case WearableItem.Bodys:
+                    return 1f;
+                case WearableItem.Shoes:
+                    return 2f;
+                case WearableItem.Pants:
+                    return 1.5f;
+                case WearableItem.Hats:
+                    return 1.2f;
+                case WearableItem.Faces:
+                    return 1.2f;
+            }
+
+            return 1f;
+        }
+
+        private GameObject CurrentTab()
+        {
+            try
+            {
+                var itemType = GameObject.FindWithTag("ItemContainer").transform.GetChild(0)
+                    .GetComponent<WearableItemController>().ItemType;
+                var tabBar = GameObject.FindWithTag("TabBar").transform;
+
+                for (var i = 0; i < 5; i++)
+                    if (tabBar.GetChild(i).GetComponent<TabItemController>().ItemType == itemType)
+                        return tabBar.GetChild(i).gameObject;
+            }
+            catch (UnityException)
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
