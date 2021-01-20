cf66db0d9867532c44f0393bd6debfb6f8443476
dreamz1e <61279526+dreamz1e@users.noreply.github.com>
2021-01-20 12:27:44 +0100

    Fixed Bug where Trash is clickable through UI

---
+++ ObjectClickController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ObjectClickController : MonoBehaviour
+{
+    private GameObject _player;
+    private Canvas _mainCanvas;
+    private BalanceController _balance;
+    private RaycastHit2D _hit;
+    private GameObject _hitObject;
+    private static decimal _multiplier = 1;
+    private TrashController _trash;
+
+    private void Start()
+    {
+        _mainCanvas = GameObject.Find("UICanvas").GetComponent<Canvas>();
+        _player = GameObject.FindWithTag("Player");
+        _balance = (BalanceController) _player.GetComponent(typeof(BalanceController));
+    }
+
+
+    private void Update()
+    {
+        if (!PauseMenuController.GetGameIsPaused() && !ShopController.IsPointerOverUI())
+        {
+            GetClickedObject(); 
+        }
+    }
+
+    private void GetClickedObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (_hit.collider != null)
+            {
+                _hitObject = _hit.collider.gameObject;
+                if(_hitObject == _player)
+                {
+                    
+                }
+                else
+                {
+                    if (_hitObject.GetComponent<Animation>().isPlaying) return;
+                    _trash = _hitObject.GetComponent<TrashController>();
+                    AddValue(Convert.ToDecimal(_trash.GetCurrencyValue()) * _multiplier);
+                    AddValueText(_trash);
+                    StartCoroutine(DestroyObject(_hitObject));
+                }
+            }
+        }
+    }
+
+    private void AddValue(decimal value)
+    {
+        _balance.AddBalance(value);
+    }
+
+    public static IEnumerator DestroyObject(GameObject objectToDestroy)
+    {
+        objectToDestroy.GetComponentInChildren<ParticleSystem>().Play();
+        objectToDestroy.GetComponent<Animation>().Play();
+        TrashManager.IncreaseCount(objectToDestroy.name, objectToDestroy.GetComponent<TrashController>().GetRarity());
+        yield return new WaitForSeconds(1f);
+        Destroy(objectToDestroy);
+    }
+
+    private void AddValueText(TrashController trash)
+    {
+        _mainCanvas.GetComponent<ValueDisplayer>().CreateText(trash);
+    }
+
+    public static void IncreaseMultiplier(decimal value)
+    {
+        _multiplier = value;
+    }
+
+    public static decimal GetMultiplier()
+    {
+        return _multiplier;
+    }
+}
---
+++ ShopController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonPrefab;
+
+    [SerializeField] private ShopItemScriptableObject[] shopItems;
+
+    private readonly List<Button> _buttons = new List<Button>();
+
+    private readonly List<decimal> _costIncrements = new List<decimal>();
+    private readonly List<decimal> _multiplierIncrement = new List<decimal>();
+
+    private readonly List<int> _upgradeClickLevel = new List<int>();
+    private readonly List<decimal> _upgradeCosts = new List<decimal>();
+    private readonly List<UpgradeType> _upgradeTypes = new List<UpgradeType>();
+
+    private decimal _activeMultiplier;
+    private BalanceController _bc;
+
+    private Transform _buttonParent;
+    private MultiplierDisplayController _mdc;
+
+    private GameObject _mpDispay;
+
+    private GameObject _player;
+
+
+    private void Start()
+    {
+        _buttonParent = GameObject.FindWithTag("ButtonContainer").transform;
+        _player = GameObject.FindWithTag("Player");
+        _mpDispay = GameObject.FindWithTag("MultiplierText");
+        _bc = _player.GetComponent<BalanceController>();
+        _mdc = _mpDispay.GetComponent<MultiplierDisplayController>();
+        _activeMultiplier = ObjectClickController.GetMultiplier();
+        shopItems = AssignItemsToArray();
+        InstantiateButtons();
+    }
+
+    public void Upgrade(int value)
+    {
+        if (GetBalance() >= _upgradeCosts[value] && _upgradeTypes[value] == UpgradeType.ClickUpgrade)
+            DoClickUpgrade(value);
+    }
+
+    private void DoClickUpgrade(int value)
+    {
+        _upgradeClickLevel[value] += 1;
+        _bc.AddBalance(-_upgradeCosts[value]);
+        ModifyUpgradeCost(value);
+        ModifyCurrentMultiplier(value);
+        UpdateUpgradeDisplay(value);
+        _mdc.UpdateDisplay();
+    }
+
+    private void ModifyCurrentMultiplier(int value)
+    {
+        var newMultiplier = _activeMultiplier + _multiplierIncrement[value];
+        ObjectClickController.IncreaseMultiplier(newMultiplier);
+        _activeMultiplier = newMultiplier;
+    }
+
+    private void ModifyUpgradeCost(int value)
+    {
+        _upgradeCosts[value] = Math.Round(_upgradeCosts[value] * _costIncrements[value]);
+    }
+
+    private void UpdateUpgradeDisplay(int value)
+    {
+        _buttons[value].GetComponentInChildren<Text>().text = string.Format(shopItems[value].ButtonText,
+            NumberShortener.ShortenNumber(_upgradeCosts[value]),
+            _upgradeClickLevel[value],
+            _multiplierIncrement[value]);
+    }
+
+    public List<decimal> GetUpgradeCosts()
+    {
+        return _upgradeCosts;
+    }
+
+    public decimal GetBalance()
+    {
+        return _bc.GetBalance();
+    }
+
+    public List<Button> GetButtons()
+    {
+        return _buttons;
+    }
+
+    private ShopItemScriptableObject[] AssignItemsToArray()
+    {
+        var objectList = Resources.LoadAll<ShopItemScriptableObject>("ShopItems");
+        Array.Sort(objectList, (item1, item2) => item1.ButtonNumber.CompareTo(item2.ButtonNumber));
+        return objectList;
+    }
+
+    private void InstantiateButtons()
+    {
+        var index = 0;
+        foreach (var item in shopItems)
+        {
+            var instantiatedObject = Instantiate(buttonPrefab, _buttonParent);
+            var instanceDirections = instantiatedObject.GetComponent<RectTransform>();
+            _buttons.Add(instantiatedObject.GetComponent<Button>());
+            instanceDirections.localScale = new Vector3(1f, 1f, 1f);
+            _buttons[index].GetComponentInChildren<Text>().text = string.Format(item.ButtonText,
+                NumberShortener.ShortenNumber(item.UpgradeCosts),
+                item.UpgradeLevel,
+                item.MultiplierIncrement);
+            InstantiateValues(item);
+            SetButtonsItemImage(item, _buttons[index]);
+            index++;
+        }
+
+        AddingListeners();
+    }
+
+    private void InstantiateValues(ShopItemScriptableObject item)
+    {
+        _upgradeClickLevel.Add(item.UpgradeLevel);
+        _multiplierIncrement.Add(Convert.ToDecimal(item.MultiplierIncrement));
+        _upgradeCosts.Add(Convert.ToDecimal(item.UpgradeCosts));
+        _costIncrements.Add(Convert.ToDecimal(item.CostIncrements));
+        _upgradeTypes.Add(item.Type);
+    }
+
+    private void AddingListeners()
+    {
+        for (var i = 0; i < _buttons.Count; i++)
+        {
+            var tempInt = i;
+            _buttons[i].onClick.AddListener(delegate { Upgrade(tempInt); });
+        }
+    }
+
+    private void SetButtonsItemImage(ShopItemScriptableObject item, Button button)
+    {
+        var itemImage = button.transform.GetChild(1).gameObject;
+        itemImage.GetComponent<Image>().sprite = item.ItemImage;
+        itemImage.GetComponent<Image>().color = item.Color;
+    }
+
+    public static bool IsPointerOverUI()
+    {
+        PointerEventData curPos = new PointerEventData(EventSystem.current);
+        curPos.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(curPos, results);
+        return results.Count > 0;
+    }
+}
